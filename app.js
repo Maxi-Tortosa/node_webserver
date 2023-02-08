13eaa8b3a665d83b999c3eb33be80b38a5f11c3e
MaxiTortosa <maximilianotortosa@gmail.com>
2023-02-08 12:21:11 -0300

    Add handlebars dependency and configure views

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
 const express = require('express');
+var hbs = require('hbs');
+
 const app = express();
 const port = 8090;
+
+//Setear handlebars
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 //Servir contenido estático
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.send('Home page');
-});
-
-app.get('/holamundo', (req, res) => {
-	res.send('En su respectiva ruta');
-});
-
-app.get('/generic', (req, res) => {
-	res.sendFile(__dirname + '/public/generic.html');
+	res.render('home', { titulo: 'Curso node', nombre: 'Maximiliano Tortosa' });
 });
 
 app.get('/elements', (req, res) => {
-	res.sendFile(__dirname + '/public/elements.html');
+	res.render('elements', {
+		titulo: 'Curso node',
+		nombre: 'Maximiliano Tortosa',
+	});
 });
+
+app.get('/generic', (req, res) => {
+	res.render('generic', {
+		titulo: 'Curso node',
+		nombre: 'Maximiliano Tortosa',
+	});
+});
+
+// app.get('/holamundo', (req, res) => {
+// 	res.send('En su respectiva ruta');
+// });
+
+// app.get('/generic', (req, res) => {
+// 	res.sendFile(__dirname + '/public/generic.html');
+// });
+
+// app.get('/elements', (req, res) => {
+// 	res.sendFile(__dirname + '/public/elements.html');
+// });
 
 app.get('*', (req, res) => {
 	res.sendFile(__dirname + '/public/404.html');
